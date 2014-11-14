10f3bdebf34d2f771070c6e99b48d373402a4ec5
Michael Andersen <m.andersen@cs.berkeley.edu>
2014-11-13 18:30:47 -0800

    Added image copy

--- mkBIN.sh
+++ mkBIN.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-petalinux-package --boot --fsbl ./images/linux/zynq_fsbl.elf --fpga ./hardware/Avnet-MicroZed-z7010-2014.2/Avnet-MicroZed-z7010-2014.2.runs/impl_1/zed_wrapper.bit --uboot
+petalinux-package --force --boot --fsbl ./images/linux/zynq_fsbl.elf --fpga ./hardware/Avnet-MicroZed-z7010-2014.2/Avnet-MicroZed-z7010-2014.2.runs/impl_1/zed_wrapper.bit --uboot
+cp images/linux/image.ub $EdXCEL_SDCARD/
 cp BOOT.BIN $EdXCEL_SDCARD/ 
+sync
