d897c32678ae10c24666336bde62790abcfd484e
Kaifei Chen <kaifei@berkeley.edu>
2014-11-14 23:39:01 -0800

    Add load_3 and load_4
    - Add load_3 and load_4
    - Add load_3_tb and load_4_tb

--- verilog/src/fe/load_4.v
+++ verilog/src/fe/load_4.v
@@ -1,9 +1,10 @@
-module load_3 (
+module load_4 (
     clk, 
     rst,
     s0,
     s1,
     s2,
+    s3,
     result
     );
 
@@ -12,18 +13,20 @@ input rst;
 input [7:0] s0;
 input [7:0] s1;
 input [7:0] s2;
+input [7:0] s3;
 output [63:0] result;
 wire clk;
 wire rst;
 wire [7:0] s0;
 wire [7:0] s1;
 wire [7:0] s2;
+wire [7:0] s3;
 reg [63:0] result;
 
 
 always @ (posedge clk)
 begin
-    result = s0 | (s1 << 8) | (s2 << 16);
+    result = s0 | (s1 << 8) | (s2 << 16) | (s3 << 24);
 end
 
 endmodule
