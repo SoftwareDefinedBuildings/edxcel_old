664d8786ab2736e78ad6c8fb0fe0b2c7ffc8f8b0
Kaifei Chen <kaifei@berkeley.edu>
2014-11-15 22:20:39 -0800

    Remove unused functions and files for verifying
    - Change test.c to only do verify
    - Remove unused functions and files for verifying

--- verilog/c_ref/src/sc.h
+++ verilog/c_ref/src/sc.h
@@ -7,6 +7,5 @@ where l = 2^252 + 27742317777372353535851937790883648493.
 */
 
 void sc_reduce(unsigned char *s);
-void sc_muladd(unsigned char *s, const unsigned char *a, const unsigned char *b, const unsigned char *c);
 
 #endif
--- verilog/c_ref/test.c
+++ verilog/c_ref/test.c
@@ -1,150 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 
 /* #define ED25519_DLL */
 #include "src/ed25519.h"
 
-#include "src/ge.h"
-#include "src/sc.h"
-
-
 int main() {
-    unsigned char public_key[32], private_key[64], seed[32], scalar[32];
-    unsigned char other_public_key[32], other_private_key[64];
-    unsigned char shared_secret[32], other_shared_secret[32];
-    unsigned char signature[64];
-
-    clock_t start;
-    clock_t end;
-    int i;
-
+    unsigned char public_key[32] = {0x82, 0x66, 0xed, 0x57, 0x26, 0x0c, 0x99, 
+        0xfb, 0xb3, 0x91, 0x73, 0x79, 0xbc, 0xe1, 0x53, 0xb2, 0xab, 0x65, 0xfa, 
+        0x5b, 0xe2, 0x7f, 0x41, 0xf4, 0x3d, 0x65, 0xe1, 0xa1, 0x1e, 0xaf, 0x04, 
+        0x29};
+    unsigned char signature[64] = {0xcd, 0xd9, 0x0b, 0x65, 0xc7, 0x96, 0x6f, 
+        0x1c, 0xdb, 0x98, 0x6a, 0x3b, 0xd9, 0x12, 0xe4, 0xab, 0xff, 0xda, 0xcb, 
+        0xaa, 0x20, 0x2c, 0x4e, 0x3a, 0x3e, 0xd0, 0x1a, 0xb4, 0x7c, 0x99, 0xd3, 
+        0xc2, 0xae, 0xc0, 0x91, 0x7d, 0xe3, 0xfe, 0x3b, 0x54, 0xf5, 0x8c, 0x95, 
+        0x57, 0x7b, 0xaa, 0xf7, 0x4d, 0x40, 0xa2, 0x34, 0x84, 0xf9, 0x95, 0xfa, 
+        0xae, 0xdc, 0xd1, 0xf4, 0x0f, 0xbc, 0x82, 0xf3, 0x0d};
     const unsigned char message[] = "Hello, world!";
     const int message_len = strlen((char*) message);
 
-    /* create a random seed, and a keypair out of that seed */
-    ed25519_create_seed(seed);
-    ed25519_create_keypair(public_key, private_key, seed);
+    int success = ed25519_verify(signature, message, message_len, public_key);
 
-    /* create signature on the message with the keypair */
-    ed25519_sign(signature, message, message_len, public_key, private_key);
-
-    /* verify the signature */
-    if (ed25519_verify(signature, message, message_len, public_key)) {
-        printf("valid signature\n");
-    } else {
-        printf("invalid signature\n");
-    }
-
-    /* create scalar and add it to the keypair */
-    ed25519_create_seed(scalar);
-    ed25519_add_scalar(public_key, private_key, scalar);
-
-    /* create signature with the new keypair */
-    ed25519_sign(signature, message, message_len, public_key, private_key);
-
-    /* verify the signature with the new keypair */
-    if (ed25519_verify(signature, message, message_len, public_key)) {
-        printf("valid signature\n");
-    } else {
-        printf("invalid signature\n");
-    }
-
-    /* make a slight adjustment and verify again */
-    signature[44] ^= 0x10;
-    if (ed25519_verify(signature, message, message_len, public_key)) {
-        printf("did not detect signature change\n");
-    } else {
-        printf("correctly detected signature change\n");
-    }
-
-    /* generate two keypairs for testing key exchange */
-    ed25519_create_seed(seed);
-    ed25519_create_keypair(public_key, private_key, seed);
-    ed25519_create_seed(seed);
-    ed25519_create_keypair(other_public_key, other_private_key, seed);
-
-    /* create two shared secrets - from both perspectives - and check if they're equal */
-    ed25519_key_exchange(shared_secret, other_public_key, private_key);
-    ed25519_key_exchange(other_shared_secret, public_key, other_private_key);
-
-    for (i = 0; i < 32; ++i) {
-        if (shared_secret[i] != other_shared_secret[i]) {
-            printf("key exchange was incorrect\n");
-            break;
-        }
-    }
-
-    if (i == 32) {
-        printf("key exchange was correct\n");
-    }
-
-    /* test performance */
-    printf("testing seed generation performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_create_seed(seed);
-    }
-    end = clock();
-
-    printf("%fus per seed\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-
-    printf("testing key generation performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_create_keypair(public_key, private_key, seed);
-    }
-    end = clock();
-
-    printf("%fus per keypair\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    printf("testing sign performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_sign(signature, message, message_len, public_key, private_key);
-    }
-    end = clock();
-
-    printf("%fus per signature\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    printf("testing verify performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_verify(signature, message, message_len, public_key);
-    }
-    end = clock();
-
-    printf("%fus per signature\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-    
-
-    printf("testing keypair scalar addition performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_add_scalar(public_key, private_key, scalar);
-    }
-    end = clock();
-
-    printf("%fus per keypair\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    printf("testing public key scalar addition performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_add_scalar(public_key, NULL, scalar);
-    }
-    end = clock();
-
-    printf("%fus per key\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    printf("testing key exchange performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_key_exchange(shared_secret, other_public_key, private_key);
-    }
-    end = clock();
-
-    printf("%fus per shared secret\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
+    printf("success = %d\n", success);
 
     return 0;
 }
