80103c8dbb0935ba5d3fe0957104f151043e5506
Kaifei Chen <kaifei@berkeley.edu>
2014-11-16 11:33:53 -0800

    Add fe_add (not working yet)

--- verilog/src/testbed/fe_add_tb.v
+++ verilog/src/testbed/fe_add_tb.v
@@ -1,34 +1,20 @@
 module fe_add_tb();
 
-reg clk;
-reg rst;
+`include "../fe/fe_add.v"
 reg [319:0] f;
 reg [319:0] g;
-wire [319:0] h;
+reg [319:0] h;
 
 // Initialize all variables
 initial begin        
-  clk = 1;       // initial value of clock
-  rst = 0;       // initial value of reset
-  #5 rst = 1;    // Assert the reset
-  #10 rst = 0;   // De-assert the reset
-  #15 f = 320'b0;
-  g = 320'b1;
-  #100 $finish;      // Terminate simulation
+  f = 320'heddf27ff7e2105ff7c4ce4ff210354fffbe52401f0dc9dffd84c5701ad1e0100f6545c00cfa66aff;
+  g = 320'h61c7d9ffa909f6ff44be2e004d0375ff735be700e35e4f005340b700a11abcffff6790fff4a87c00;
+  #10 h = fe_add(f, g);
+  
+  //4ea701ff272bfbfec00a13006e06c9fe6e410c02d33bedff2b8d0e024e39bdfff5bcecffc34fe7ff
+  #10 $display("H is %h", h);
+  #20 $finish;      // Terminate simulation
 end
 
-// Clock generator
-always begin
-  #5 clk = ~clk; // Toggle clock every 5 ticks
-end
-
-// Connect DUT to test bench
-fe_add fe_add (
-clk,
-rst,
-f,
-g,
-h
-);
 
 endmodule
