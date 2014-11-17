4fbde54a2cbbfd9b488286d63e3a9ab59e58f634
Kaifei Chen <kaifei@berkeley.edu>
2014-11-16 18:24:57 -0800

    Move all fe functions to fe_common.v

--- verilog/src/fe/fe_common.v
+++ verilog/src/fe/fe_common.v
@@ -18,6 +18,7 @@ begin
 end
 endfunction
 
+
 function automatic [63:0] load_4;
 input [32:0] in;
 begin
@@ -25,21 +26,136 @@ begin
 end
 endfunction
 
-//Returns f + g
+
 function automatic [319:0] fe_add;
-input [319:0] f;
-input [319:0] g;
-begin
-    fe_add[ 0*32 +: 32] = $signed(f[0*32 +: 32]) + $signed(g[0*32 +: 32]);
-    fe_add[ 1*32 +: 32] = $signed(f[1*32 +: 32]) + $signed(g[1*32 +: 32]);
-    fe_add[ 2*32 +: 32] = $signed(f[2*32 +: 32]) + $signed(g[2*32 +: 32]);
-    fe_add[ 3*32 +: 32] = $signed(f[3*32 +: 32]) + $signed(g[3*32 +: 32]);
-    fe_add[ 4*32 +: 32] = $signed(f[4*32 +: 32]) + $signed(g[4*32 +: 32]);
-    fe_add[ 5*32 +: 32] = $signed(f[5*32 +: 32]) + $signed(g[5*32 +: 32]);
-    fe_add[ 6*32 +: 32] = $signed(f[6*32 +: 32]) + $signed(g[6*32 +: 32]);
-    fe_add[ 7*32 +: 32] = $signed(f[7*32 +: 32]) + $signed(g[7*32 +: 32]);
-    fe_add[ 8*32 +: 32] = $signed(f[8*32 +: 32]) + $signed(g[8*32 +: 32]);
-    fe_add[ 9*32 +: 32] = $signed(f[9*32 +: 32]) + $signed(g[9*32 +: 32]);
+input signed [319:0] f;
+input signed [319:0] g;
+begin : fe_add_blk
+    fe_add[0*32 +: 32] = f[0*32 +: 32] + g[0*32 +: 32];
+    fe_add[1*32 +: 32] = f[1*32 +: 32] + g[1*32 +: 32];
+    fe_add[2*32 +: 32] = f[2*32 +: 32] + g[2*32 +: 32];
+    fe_add[3*32 +: 32] = f[3*32 +: 32] + g[3*32 +: 32];
+    fe_add[4*32 +: 32] = f[4*32 +: 32] + g[4*32 +: 32];
+    fe_add[5*32 +: 32] = f[5*32 +: 32] + g[5*32 +: 32];
+    fe_add[6*32 +: 32] = f[6*32 +: 32] + g[6*32 +: 32];
+    fe_add[7*32 +: 32] = f[7*32 +: 32] + g[7*32 +: 32];
+    fe_add[8*32 +: 32] = f[8*32 +: 32] + g[8*32 +: 32];
+    fe_add[9*32 +: 32] = f[9*32 +: 32] + g[9*32 +: 32];
+end
+endfunction
+
+
+function automatic [319:0] fe_frombytes;
+input [255:0] s;
+begin : fe_frombytes_blk
+    reg signed [63:0] h0; 
+    reg signed [63:0] h1; 
+    reg signed [63:0] h2; 
+    reg signed [63:0] h3; 
+    reg signed [63:0] h4; 
+    reg signed [63:0] h5; 
+    reg signed [63:0] h6; 
+    reg signed [63:0] h7; 
+    reg signed [63:0] h8; 
+    reg signed [63:0] h9;
+    reg signed [63:0] carry0;
+    reg signed [63:0] carry1;
+    reg signed [63:0] carry2;
+    reg signed [63:0] carry3;
+    reg signed [63:0] carry4;
+    reg signed [63:0] carry5;
+    reg signed [63:0] carry6;
+    reg signed [63:0] carry7;
+    reg signed [63:0] carry8;
+    reg signed [63:0] carry9;
+
+    h0 = load_4(s[0*8 +: 4*8]);
+    h1 = load_3(s[4*8 +: 3*8]) << 6;
+    h2 = load_3(s[7*8 +: 3*8]) << 5;
+    h3 = load_3(s[10*8 +: 3*8]) << 3;
+    h4 = load_3(s[13*8 +: 3*8]) << 2;
+    h5 = load_4(s[16*8 +: 4*8]);
+    h6 = load_3(s[20*8 +: 3*8]) << 7;
+    h7 = load_3(s[23*8 +: 3*8]) << 5;
+    h8 = load_3(s[26*8 +: 3*8]) << 4;
+    h9 = (load_3(s[29*8 +: 3*8]) & 64'sd8388607) << 2;
+
+    carry9 = (h9 + (64'sd1 << 24)) >>> 25;
+    h0 = h0 + (carry9 * 64'sd19);
+    h9 = h9 - (carry9 << 25);
+    carry1 = (h1 + (64'sd1 << 24)) >>> 25;
+    h2 = h2 + carry1;
+    h1 = h1 - (carry1 << 25);
+    carry3 = (h3 + (64'sd1 << 24)) >>> 25;
+    h4 = h4 + carry3;
+    h3 = h3 - (carry3 << 25);
+    carry5 = (h5 + (64'sd1 << 24)) >>> 25;
+    h6 = h6 + carry5;
+    h5 = h5 - (carry5 << 25);
+    carry7 = (h7 + (64'sd1 << 24)) >>> 25;
+    h8 = h8 + carry7;
+    h7 = h7 - (carry7 << 25);
+    carry0 = (h0 + (64'sd1 << 25)) >>> 26;
+    h1 = h1 + carry0;
+    h0 = h0 - (carry0 << 26);
+    carry2 = (h2 + (64'sd1 << 25)) >>> 26;
+    h3 = h3 + carry2;
+    h2 = h2 - (carry2 << 26);
+    carry4 = (h4 + (64'sd1 << 25)) >>> 26;
+    h5 = h5 + carry4;
+    h4 = h4 - (carry4 << 26);
+    carry6 = (h6 + (64'sd1 << 25)) >>> 26;
+    h7 = h7 + carry6;
+    h6 = h6 - (carry6 << 26);
+    carry8 = (h8 + (64'sd1 << 25)) >>> 26;
+    h9 = h9 + carry8;
+    h8 = h8 - (carry8 << 26);
+
+    fe_frombytes[0*32 +: 32] = h0;
+    fe_frombytes[1*32 +: 32] = h1;
+    fe_frombytes[2*32 +: 32] = h2;
+    fe_frombytes[3*32 +: 32] = h3;
+    fe_frombytes[4*32 +: 32] = h4;
+    fe_frombytes[5*32 +: 32] = h5;
+    fe_frombytes[6*32 +: 32] = h6;
+    fe_frombytes[7*32 +: 32] = h7;
+    fe_frombytes[8*32 +: 32] = h8;
+    fe_frombytes[9*32 +: 32] = h9;
+end
+endfunction
+
+
+function automatic [319:0] fe_neg;
+input signed [319:0] f;
+begin : fe_neg_blk
+    fe_neg[0*32 +: 32] = -f[0*32 +: 32];
+    fe_neg[1*32 +: 32] = -f[1*32 +: 32];
+    fe_neg[2*32 +: 32] = -f[2*32 +: 32];
+    fe_neg[3*32 +: 32] = -f[3*32 +: 32];
+    fe_neg[4*32 +: 32] = -f[4*32 +: 32];
+    fe_neg[5*32 +: 32] = -f[5*32 +: 32];
+    fe_neg[6*32 +: 32] = -f[6*32 +: 32];
+    fe_neg[7*32 +: 32] = -f[7*32 +: 32];
+    fe_neg[8*32 +: 32] = -f[8*32 +: 32];
+    fe_neg[9*32 +: 32] = -f[9*32 +: 32];
+end
+endfunction
+
+
+function automatic [319:0] fe_sub;
+input signed [319:0] f;
+input signed [319:0] g;
+begin : fe_sub_blk
+    fe_sub[0*32 +: 32] = f[0*32 +: 32] - g[0*32 +: 32];
+    fe_sub[1*32 +: 32] = f[1*32 +: 32] - g[1*32 +: 32];
+    fe_sub[2*32 +: 32] = f[2*32 +: 32] - g[2*32 +: 32];
+    fe_sub[3*32 +: 32] = f[3*32 +: 32] - g[3*32 +: 32];
+    fe_sub[4*32 +: 32] = f[4*32 +: 32] - g[4*32 +: 32];
+    fe_sub[5*32 +: 32] = f[5*32 +: 32] - g[5*32 +: 32];
+    fe_sub[6*32 +: 32] = f[6*32 +: 32] - g[6*32 +: 32];
+    fe_sub[7*32 +: 32] = f[7*32 +: 32] - g[7*32 +: 32];
+    fe_sub[8*32 +: 32] = f[8*32 +: 32] - g[8*32 +: 32];
+    fe_sub[9*32 +: 32] = f[9*32 +: 32] - g[9*32 +: 32];
 end
 endfunction
 
@@ -226,25 +342,3 @@ begin : fe_tobytes_blk
     fe_tobytes[31*8+:8] = tmp[7:0];
 end
 endfunction
-
-
-//Returns f if b = 0 else g
-/*
-function automatic [319:0] fe_cmov
-input [319:0] f;
-input [319:0] g;
-input b;
-begin
-    if (b)
-        fe_cmov = g;
-    else
-        fe_cmov = f;
-end
-endfunction
-*/
-
-//TODO
-// fe_invert
-
-//Result is valid N clocks later
-
