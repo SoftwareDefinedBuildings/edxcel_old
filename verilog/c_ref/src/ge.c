207b6875973f5b444ce8862b59459c0a965f5ad8
Kaifei Chen <kaifei@berkeley.edu>
2014-11-18 03:30:39 +0000

    remove unnecessary part of slide

--- verilog/c_ref/src/ge.c
+++ verilog/c_ref/src/ge.c
@@ -30,31 +30,6 @@ static void slide(signed char *r, const unsigned char *a) {
     for (i = 0; i < 256; ++i) {
         r[i] = 1 & (a[i >> 3] >> (i & 7));
     }
-
-    for (i = 0; i < 256; ++i)
-        if (r[i]) {
-            for (b = 1; b <= 6 && i + b < 256; ++b) {
-                if (r[i + b]) {
-                    if (r[i] + (r[i + b] << b) <= 15) {
-                        r[i] += r[i + b] << b;
-                        r[i + b] = 0;
-                    } else if (r[i] - (r[i + b] << b) >= -15) {
-                        r[i] -= r[i + b] << b;
-
-                        for (k = i + b; k < 256; ++k) {
-                            if (!r[k]) {
-                                r[k] = 1;
-                                break;
-                            }
-
-                            r[k] = 0;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
 }
 
 /*
