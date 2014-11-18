2e0a0bedada8fff4d5cc3ee0d1a200e87f132307
Kaifei Chen <kaifei@berkeley.edu>
2014-11-18 14:23:57 -0800

    Add code for ge_frombytes_negate_vartime, NOT tested

--- verilog/c_ref/src/verify.c
+++ verilog/c_ref/src/verify.c
@@ -57,6 +57,8 @@ int ed25519_verify(const unsigned char *signature, const unsigned char *message,
     sha512_update(&hash, message, message_len);
     sha512_final(&hash, h);
 
+    sc_reduce(h);
+    
     if (signature[63] & 224) {
         return 0;
     }
@@ -65,7 +67,6 @@ int ed25519_verify(const unsigned char *signature, const unsigned char *message,
         return 0;
     }
     
-    sc_reduce(h);
     ge_double_scalarmult_vartime(&R, h, &A, signature + 32);
     ge_tobytes(checker, &R);
 
