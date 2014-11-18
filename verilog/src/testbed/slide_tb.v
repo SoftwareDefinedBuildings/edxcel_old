ad1eeede5d98761efd8d061a149e16b5f4c6e192
Kaifei Chen <kaifei@cs.berkeley.edu>
2014-11-17 19:45:27 -0800

    Update slide implementation for simplified slide in C codes!

--- verilog/src/testbed/slide_tb.v
+++ verilog/src/testbed/slide_tb.v
@@ -1,42 +1,18 @@
 module slide_tb();
 
+`include "../ge/slide.v"
+
 reg [0:32*8-1] a;
-reg clk;
-reg rst;
-reg valid;
-wire signed [0:256*8-1] r;
-wire done;
+reg signed [0:256*8-1] r;
 
 
+// Initialize all variables
 initial begin
-  clk = 0;
-  rst = 1;
-  #10 rst = 0;
-  #10 rst = 1;
-  #20 a = 320'h42e6b3da746f7982c72e3945ca2e987362ae2af25aa079470fd723b60d430c0a;
-  
-  valid = 1;
-  #10 valid = 0;
+  a = 256'h42e6b3da746f7982c72e3945ca2e987362ae2af25aa079470fd723b60d430c0a;
+  #100 r = slide(a);
+  #10000 $display("r is %h", r);
+  #20000 $finish;      // Terminate simulation
 end
 
-always @ (done) begin
-  if (done === 1'b1) begin
-      $display("r is %h", r);
-      $finish;
-  end
-end
-
-always begin
- #5 clk = !clk;
-end
-
-slide slide(
-clk,
-rst,
-valid,
-a,
-r,
-done
-);
 
 endmodule
