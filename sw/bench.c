a0e6ba7fd3f3a3392d76732ff4071c347f550286
Kaifei Chen <kaifei@berkeley.edu>
2014-11-19 09:26:17 +0000

    add some codes for test bench in C on ARM

--- sw/bench.c
+++ sw/bench.c
@@ -14,114 +14,177 @@
 #define SIGNATURE_LEN 64
 #define KEY_LEN 32
 
-#define MESSAGE_WORDS 	(MESSAGE_LEN/4)
+#define MESSAGE_WORDS     (MESSAGE_LEN/4)
 #define SIGNATURE_WORDS (SIGNATURE_LEN/4)
-#define KEY_WORDS 		(KEY_LEN/4)
+#define KEY_WORDS         (KEY_LEN/4)
 
 //Return 1 if sig is ok, 0 if sig fails
 inline int sw_verify(uint8_t *sig, uint8_t *msg, uint8_t *key)
 {
-	return ed25519_verify(sig, msg, 32, key);
+    return ed25519_verify(sig, msg, 32, key);
 }
 
 uint8_t hw_verify_sync(uint8_t *sig, uint8_t *msg, uint8_t *key)
 {
-	//Find available EPU
-	int i;
-	uint32_t result;
-	//uint8_t epu_idx = XL_AVAILABLE_IDX;
-	uint8_t epu_idx = 0;
-	assert (epu_idx != XL_AVAILABLE_NONE);
-	
-	
-	//Load signature
-	for (i=0;i<SIGNATURE_WORDS;i++)
-		XL_EPU_SIG(epu_idx)[i] = ((uint32_t*)&sig[0])[i];
-	
-	//Load message
-	for (i=0;i<MESSAGE_WORDS;i++)
-		XL_EPU_MSG(epu_idx)[i] = ((uint32_t*)&msg[0])[i];
-		
-	//Load key	
-	for (i=0;i<KEY_WORDS;i++)
-		XL_EPU_KEY(epu_idx)[i] = ((uint32_t*)&key[0])[i];
-	
-	//Start computation
-	XL_EPU_GO(epu_idx) = XL_GO_CODE;
-		
-	//Wait until calculation is done
-	while(!XL_EPU_DONE(epu_idx));
+    //Find available EPU
+    int i;
+    uint32_t result;
+    //uint8_t epu_idx = XL_AVAILABLE_IDX;
+    uint8_t epu_idx = 0;
+    assert (epu_idx != XL_AVAILABLE_NONE);
+    
+    
+    //Load signature
+    for (i=0;i<SIGNATURE_WORDS;i++)
+        XL_EPU_SIG(epu_idx)[i] = ((uint32_t*)&sig[0])[i];
+    
+    //Load message
+    for (i=0;i<MESSAGE_WORDS;i++)
+        XL_EPU_MSG(epu_idx)[i] = ((uint32_t*)&msg[0])[i];
+        
+    //Load key    
+    for (i=0;i<KEY_WORDS;i++)
+        XL_EPU_KEY(epu_idx)[i] = ((uint32_t*)&key[0])[i];
+    
+    //Start computation
+    XL_EPU_GO(epu_idx) = XL_GO_CODE;
+        
+    //Wait until calculation is done
+    while(!XL_EPU_DONE(epu_idx));
 
-	result = XL_EPU_DONE(epu_idx);
-	printf("result was: %d\n", result);
-	result = XL_EPU_DONE(epu_idx);
-	printf("result was: %d\n", result);
-	//Codes are 0 for busy, 1 for sig pass and 2 for sig fail
-	return result == 1;
+    result = XL_EPU_DONE(epu_idx);
+    printf("result was: %d\n", result);
+    result = XL_EPU_DONE(epu_idx);
+    printf("result was: %d\n", result);
+    //Codes are 0 for busy, 1 for sig pass and 2 for sig fail
+    return result == 1;
 }
 
 void enumerate_API()
 {
-	int i;
-	
-	for (i = 0;i<XL_NUM_EPU;i++)
-	{
-		
-	}
+    int i;
+    
+    for (i = 0;i<XL_NUM_EPU;i++)
+    {
+        
+    }
 }
 int check_ok_sig()
 {
-	uint8_t sig [64] = {0x55,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   , 		
-						0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   };
-	uint8_t key [32] = {0x5a,0x5a,0x5a,0x5a,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0x5a,0x5a,0x5a,0x5a,0x5a};
-	uint8_t msg [32] = {0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   };
-	int result = hw_verify_sync(sig, msg, key);
-	return result;
+    uint8_t sig [64] = {0x55,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    uint8_t key [32] = {0x5a,0x5a,0x5a,0x5a,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0x5a,0x5a,0x5a,0x5a,0x5a};
+    uint8_t msg [32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int result = hw_verify_sync(sig, msg, key);
+    return result;
 }
 
 int check_bad_sig()
 {
-	uint8_t sig [64] = {8,8,7,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,50,0,0,0,0,0,0,0,0,0,37,0,0,0,0,0,50};
-	uint8_t key [32] = {															80,80,80,80,0,0,0,0,0,0,0,0,0,0,0,50,0,0,0,0,0,0,0,0,0, 0,0,0,80,80,80,80};
-	uint8_t msg [32] = {														3,4,5,6,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,38,0,0,4,3,2,1};
-	int result = hw_verify_sync(sig, msg, key);
-	return result;
+    uint8_t sig [64] = {8,8,7,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,50,0,0,0,0,0,0,0,0,0,37,0,0,0,0,0,50};
+    uint8_t key [32] = {80,80,80,80,0,0,0,0,0,0,0,0,0,0,0,50,0,0,0,0,0,0,0,0,0, 0,0,0,80,80,80,80};
+    uint8_t msg [32] = {3,4,5,6,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,38,0,0,4,3,2,1};
+    int result = hw_verify_sync(sig, msg, key);
+    return result;
 }
 
 volatile uint32_t* dest;
 
-void go()
+int read_data(char **pks, char **msgs, char **sigs, long limit)
 {
-	uint32_t* p = (XL_EPU_KEY(0));
-	dest = malloc(64*4);
-	uint32_t i, j;
-	unsigned long int then, now;
-	then = clock();
-	for (i=0;i<10000;i++)
-	{
-		for (j = 0; j<64/4; j++)
-		{
-			p[j] = i;
-		}
-	}
-	now = clock();
-	printf("To PL %lu\n", now-then);
-	then = clock();
-	for (i=0;i<10000;i++)
-	{
-		for (j = 0; j<64/4; j++)
-		{
-			dest[j] = i;
-		}
-	}
-	now = clock();
-	printf("To Mem %lu\n", now-then);
-/*
-	printf("ok sig result: %d\n", check_ok_sig());
-	printf("bad sig result: %d\n", check_bad_sig());
-	printf("ok sig result: %d\n", check_ok_sig());
-	printf("bad sig result: %d\n", check_bad_sig());
-	*/
+    int i;
+    long count = 0;
+
+    if (pks == NULL || msgs == NULL || sigs == NULL)
+        return 0;
+
+    while (1) {
+        char *pos;
+
+        if (count >= limit)
+            return count;
+
+        if (scanf("%s", str) == EOF)
+            return count;
+
+        char *pk = malloc(32 * sizeof(char));
+        if (pk == NULL)
+            return count;
+        char *msg = malloc(32 * sizeof(char));
+        if (msg == NULL) {
+            free(pk);
+            return count;
+        }
+        char *sig = malloc(64 * sizeof(char));
+        if (sig == NULL) {
+            free(pk);
+            free(msg);
+            return count;
+        }
+
+        pos = str + 3; // get rid of heading of each line
+        for (i = 0; i < 32; i++) {
+            sscanf(pos, "%2hhx", &pk[j]);
+            pos += 2 * sizeof(char);
+        }
+    
+        scanf("%s", str);
+        pos = str + 3;
+        for (i = 0; i < 32; i++) {
+            sscanf(pos, "%2hhx", &msg[j]);
+            pos += 2 * sizeof(char);
+        }
+        
+        scanf("%s", str);
+        pos = str + 4;
+        for (i = 0; i < 64; i++) {
+            sscanf(pos, "%2hhx", &sig[j]);
+            pos += 2 * sizeof(char);
+        }
+        
+    }
+
+    return 0;
 }
 
+        // int result = hw_verify_sync(sig, msg, key);
+        // 
+        // count_total++;
+        // if (success) count_success++;
+        // printf("success = %d (%d/%d)\n", success, count_success, count_total);
+
+void go()
+{
+    uint32_t* p = (XL_EPU_KEY(0));
+    dest = malloc(64*4);
+    uint32_t i, j;
+    unsigned long int then, now;
+    then = clock();
+    for (i=0;i<10000;i++)
+    {
+        for (j = 0; j<64/4; j++)
+        {
+            p[j] = i;
+        }
+    }
+    now = clock();
+    printf("To PL %lu\n", now-then);
+    then = clock();
+    for (i=0;i<10000;i++)
+    {
+        for (j = 0; j<64/4; j++)
+        {
+            dest[j] = i;
+        }
+    }
+    now = clock();
+    printf("To Mem %lu\n", now-then);
+/*
+    printf("ok sig result: %d\n", check_ok_sig());
+    printf("bad sig result: %d\n", check_bad_sig());
+    printf("ok sig result: %d\n", check_ok_sig());
+    printf("bad sig result: %d\n", check_bad_sig());
+    */
+}
 
